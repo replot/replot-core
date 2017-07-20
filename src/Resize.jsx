75e49af98f2fcc187e093298e324d94571c5e6f1
Matt Farejowicz <mfarejowicz@gmail.com>
2017-07-20 15:17:23 -0400

    fix to width input issues

--- src/Resize.jsx
+++ src/Resize.jsx
@@ -7,73 +7,65 @@ class Resize extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      width: parseInt(this.props.children.props.width),
       resizing: false
     }
   }
 
   parseWidth(width){
-    if (typeof(width) === "number") {
+    if (typeof(width) === "number" || !width.includes("%")) {
       return {dynamic: false, scale: 1}
-    }
-    else if (width.includes("%")){
-      return {dynamic: true, scale: parseInt(width) / 100}
     }
     else {
-      return {dynamic: false, scale: 1}
+      return {dynamic: true, scale: parseInt(width) / 100}
     }
   }
 
   componentDidMount() {
-    if (this.parseWidth(this.props.children.props.width).dynamic) {
-      this.updateDimensions(this.props.children.props.width, this.elem.parentNode)
-      window.addEventListener("resize", this.resize.bind(this, this.elem.parentNode))
+    window.addEventListener("resize", this.resize.bind(this))
+  }
+
+  resize() {
+    if (this.parseWidth(this.props.width).dynamic){
+      this.setState({
+        resizing: true
+      })
+      var updateFunction
+      clearTimeout(updateFunction)
+      updateFunction = setTimeout(this.updateDimensions.bind(this), 1200)
     }
   }
 
-  resize(comp) {
+  updateDimensions() {
     this.setState({
-      resizing: true
-    })
-    var updateFunction
-    clearTimeout(updateFunction)
-    updateFunction = setTimeout(this.updateDimensions.bind(this, this.props.children.props.width, comp), 1200)
-  }
-
-  updateDimensions(width, comp) {
-    this.setState({
-      resizing: false,
-      width: (this.parseWidth(width).dynamic ? comp.clientWidth * this.parseWidth(width).scale : parseInt(width))
+      resizing: false
     })
   }
 
   render() {
+    let width
+    if (!this.parseWidth(this.props.width).dynamic) {
+      width = parseInt(this.props.width)
+    } else {
+      width = this.elem.parentNode.clientWidth * this.parseWidth(this.props.width).scale
+    }
+
     if (this.state.resizing) {
       return (
-        <div>
-          <LoadingIcon width={this.state.width}/>
+        <div ref={(comp) => {this.elem = comp}}>
+          <LoadingIcon width={width}/>
         </div>
       )
     }
 
-    let child = React.cloneElement(this.props.children, {width: this.state.width})
+    let child = React.cloneElement(this.props.children, {width: width})
 
     return (
-      <div ref={(comp) => {this.elem = comp}} style={{width: `${this.state.width}px`}}>
+      <div ref={(comp) => {this.elem = comp}} style={{width: `${width}px`}}>
         {child}
       </div>
     )
   }
 
-  componentWillReceiveProps(nextProps){
-    if (this.parseWidth(nextProps.children.props.width).dynamic){
-      this.updateDimensions(nextProps.children.props.width, this.elem.parentNode)
-      window.addEventListener("resize", this.resize.bind(this, this.elem.parentNode))
-    } else {
-      window.removeEventListener("resize", this.resize.bind(this, this.elem.parentNode)) //This appears to do nothing
-      this.setState({width: parseInt(nextProps.children.props.width)})
-    }
-  }
 }
 
 export default Resize
