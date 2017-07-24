1abfcfdf1040e58df29afff1c7a8add4a434a07a
Matt Farejowicz <mfarejowicz@gmail.com>
2017-07-24 14:19:45 -0400

    add removeEventListener for when the component unmounts

--- src/Resize.jsx
+++ src/Resize.jsx
@@ -4,7 +4,7 @@ import LoadingIcon from "./LoadingIcon.jsx"
 
 class Resize extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       width: parseInt(this.props.width),
@@ -12,17 +12,19 @@ class Resize extends React.Component {
     }
   }
 
-  parseWidth(width){
+  parseWidth(width) {
     if (typeof(width) === "number" || !width.includes("%")) {
       return {dynamic: false, scale: 1}
-    }
-    else {
-      return {dynamic: true, scale: parseInt(width) / 100}
+    } else {
+      return {
+        dynamic: true,
+        scale: parseInt(width) / 100
+      }
     }
   }
 
   resize() {
-    if (this.parseWidth(this.props.width).dynamic){
+    if (this.parseWidth(this.props.width).dynamic) {
       this.setState({
         resizing: true,
         width: this.elem.parentNode.clientWidth * parseInt(this.props.width) / 100
@@ -60,21 +62,30 @@ class Resize extends React.Component {
 
   componentDidMount() {
     if (this.parseWidth(this.props.width).dynamic) {
-      this.setState ({width: this.elem.parentNode.clientWidth * parseInt(this.props.width) / 100})
+      this.setState({
+        width: this.elem.parentNode.clientWidth * parseInt(this.props.width) / 100
+      })
     }
-    window.addEventListener("resize", this.resize.bind(this))
+    this.listener = this.resize.bind(this)
+    window.addEventListener("resize", this.listener)
   }
 
-  componentWillReceiveProps(nextProps){
-    if (this.props != nextProps){
+  componentWillReceiveProps(nextProps) {
+    if (this.props != nextProps) {
       if (this.parseWidth(nextProps.width).dynamic) {
-        this.setState ({width: ((this.elem.parentNode.clientWidth * parseInt(nextProps.width) / 100) > 0
-          ? this.elem.parentNode.clientWidth * parseInt(nextProps.width) / 100
-          : 1)})
+        this.setState({
+          width: ((this.elem.parentNode.clientWidth * parseInt(nextProps.width) / 100) > 0
+            ? this.elem.parentNode.clientWidth * parseInt(nextProps.width) / 100
+            : 1)
+        })
       } else {
         this.setState({width: parseInt(nextProps.width)})
       }
     }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.listener)
   }
 
 }
