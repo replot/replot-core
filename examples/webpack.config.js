3d2e4c2480def16b619d416302c9dd33fc6a3777
Matt Farejowicz <mfarejowicz@gmail.com>
2017-07-24 15:45:29 -0400

    set up legend work

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -4,7 +4,8 @@ module.exports = function () {
       example: "./example.jsx",
       example2: "./example2.jsx",
       example3: "./example3.jsx",
-      example_loading: "./example_loading.jsx"
+      example_loading: "./example_loading.jsx",
+      example_legend: "./example_legend.jsx"
     },
     output: {
       path: __dirname + "/static",
