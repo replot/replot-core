8e4774b4e0d880a54a7a58001dbd653faf8b1dbb
Alisa Ono <alisao@mit.edu>
2018-02-07 19:00:52 -0500

    Updated dependencies, exports in index.js

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import Tooltip from "./Tooltip.jsx"
-import Resize from "./Resize.jsx"
-import LoadingIcon from "./LoadingIcon.jsx"
-import Legend from "./Legend.jsx"
-import Axis from "./Axis.jsx"
-import ColorPalette from "./ColorPalette.js"
-import GradientsContainer from "./GradientsContainer.jsx"
-
-export {Tooltip, Resize, LoadingIcon, Legend, Axis, ColorPalette, GradientsContainer}
